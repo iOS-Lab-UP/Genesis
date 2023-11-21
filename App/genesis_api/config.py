4b71c81669c2172f89a3c3bec5f2fd3adb51db30
luisced <luis.cedillo.maldonado@gmail.com>
2023-11-21 12:33:10 -0600

    Added medicine db

--- App/genesis_api/config.py
+++ App/genesis_api/config.py
@@ -19,8 +19,9 @@ class Config:
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
     MYSQL_PORT = os.getenv("MYSQL_PORT")
-    
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI",f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "SQLALCHEMY_DATABASE_URI", f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
@@ -37,21 +38,23 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
     # URL for redis
-    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL", 'redis://redis_rate_limiting:6379')
+    RATELIMIT_STORAGE_URL = os.environ.get(
+        "REDIS_URL", 'redis://redis_rate_limiting:6379')
 
     # URL for uploading pdfs
     UPLOAD_FOLDER = os.environ.get(
         "UPLOAD_FOLDER", 'App/genesis_api/static/uploads')
-    
-    REDIS_CLIENT = redis.StrictRedis(host='redis_sessions', port=6379, decode_responses=True, db=0)
-    REDIS_JWT_CLIENT = redis.StrictRedis(host='redis_sessions', port=6379, db=1, decode_responses=True)
 
+    REDIS_CLIENT = redis.StrictRedis(
+        host='redis_sessions', port=6379, decode_responses=True, db=0)
+    REDIS_JWT_CLIENT = redis.StrictRedis(
+        host='redis_sessions', port=6379, db=1, decode_responses=True)
 
     CACHE_CONFIG = {
-    "DEBUG": True,          # some Flask specific configs
-    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 300
-}
+        "DEBUG": True,          # some Flask specific configs
+        "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+        "CACHE_DEFAULT_TIMEOUT": 300
+    }
 
     # check if the folder exists
     if not os.path.exists(UPLOAD_FOLDER):
